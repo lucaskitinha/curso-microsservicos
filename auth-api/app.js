4bad7dfbff87b1eba38e3b915f6ebe5b31079e30
lucaskitinha <lucas.lmso@gmail.com>
2024-09-01 19:46:38 -0300

    feat: criando conexao com banco de dados auth-api

--- auth-api/app.js
+++ auth-api/app.js
@@ -1,8 +1,16 @@
 import express from "express";
 
+import * as db from "./src/config/db/initialData.js";
+import UserRoutes from "./src/modules/user/routes/UserRoutes.js"
+
 const app = express();
 const env = process.env;
 const PORT = env.PORT || 8080;
+
+db.createInitialDate();
+
+app.use(express.json());
+app.use(UserRoutes);
 
 app.get('/api/status', (req,res) => {
     return res.status(200).json({
